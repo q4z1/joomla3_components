093e8a0ee6a86421fdebd0c1256257d5100405dc
supinf0 <f0x01@web.de>
2016-11-30 23:17:25 +0100

    generate html table

--- com_pthranking/media/js/pthleader.js
+++ com_pthranking/media/js/pthleader.js
@@ -3,6 +3,20 @@ var usernameValid = false;
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
      alert('Hello earth');
+     jQuery.get("?option=com_pthranking&task=webservice&format=raw&pthtype=rankingtable").done(
+       function(data){
+         console.log("data = "+data);
+//          alert(data);
+         var obj = JSON.parse(data);
+//          jQuery('#ranking_table').html(data);
+         buildHtmlTable('#ranking_table',obj);
+
+
+       }
+     );
+
+ 
+        
 //       jQuery("#pthreg").click(function(event){
 //           jQuery('#errors').empty();
 //           usernameValid = false;
@@ -40,4 +54,48 @@ document.onreadystatechange = () => {
 //       });
   }
 };
+
+
+// var myList = [{"name" : "abc", "age" : 50},
+//               {"age" : "25", "hobby" : "swimming"},
+//               {"name" : "xyz", "hobby" : "programming"}];
+
+// Builds the HTML Table out of myList.
+function buildHtmlTable(selector,myList) {
+    var columns = addAllColumnHeaders(myList, selector);
+
+    for (var i = 0 ; i < myList.length ; i++) {
+        var row$ = jQuery('<tr/>');
+        for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+            var cellValue = myList[i][columns[colIndex]];
+
+            if (cellValue == null) { cellValue = ""; }
+
+            row$.append(jQuery('<td/>').html(cellValue));
+        }
+        jQuery(selector).append(row$);
+    }
+}
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records
+function addAllColumnHeaders(myList, selector)
+{
+    var columnSet = [];
+    var headerTr$ = jQuery('<tr/>');
+
+    for (var i = 0 ; i < myList.length ; i++) {
+        var rowHash = myList[i];
+        for (var key in rowHash) {
+            if (jQuery.inArray(key, columnSet) == -1){
+                columnSet.push(key);
+                headerTr$.append(jQuery('<th/>').html(key));
+            }
+        }
+    }
+    jQuery(selector).append(headerTr$);
+
+    return columnSet;
+}
 
--- com_pthranking/site/views/pthranking/tmpl/leaderboard.php
+++ com_pthranking/site/views/pthranking/tmpl/leaderboard.php
@@ -12,12 +12,15 @@ defined('_JEXEC') or die('Restricted access');
 $document = JFactory::getDocument();
 $document->addScript(JUri::root() . 'media/com_pthranking/js/pthleader.js');
 // $document->addStyleSheet(JUri::root() . 'media/com_pthranking/css/pthranking.css');
-// $uri = JUri::getInstance();
-// $base = $uri->toString(array('scheme', 'user', 'pass', 'host', 'port'));
+$uri = JUri::getInstance();
+$base = $uri->toString(array('scheme', 'user', 'pass', 'host', 'port'));
 // $url = $base . JRoute::_('index.php?option=com_pthranking&view=activategame', false);
 ?>
 
-<h1> Hello world</h1>
+<h1>Ranking</h1>
 
+<table id="ranking_table" border=1>
+
+</table>
 <?php echo $this->msg; ?>
 
